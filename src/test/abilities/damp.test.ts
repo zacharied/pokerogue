eb889e7ce0b2772717d9dbe97c73718bd877484b
Zach Day <zachdayz@gmail.com>
2024-06-15 03:21:38 -0400

    Adds test for damp visual functionality

--- src/test/abilities/damp.test.ts
+++ src/test/abilities/damp.test.ts
@@ -2,10 +2,7 @@ import { Abilities } from "#app/enums/abilities.js";
 import { Moves } from "#app/enums/moves";
 import { Species } from "#app/enums/species";
 import * as overrides from "#app/overrides";
-import
-{
-  MovePhase, TurnEndPhase
-} from "#app/phases";
+import { TurnEndPhase } from "#app/phases";
 import GameManager from "#app/test/utils/gameManager";
 import { getMovePosition } from "#app/test/utils/gameManagerUtils";
 import Phaser from "phaser";
@@ -45,11 +42,51 @@ describe("Abilities - Damp", () => {
 
     game.doAttack(getMovePosition(game.scene, 0, moveToUse));
 
-    await game.phaseInterceptor.to(MovePhase);
+    await game.phaseInterceptor.to(TurnEndPhase);
+
+    expect(game.phaseInterceptor.log).toContain("ShowAbilityPhase");
+  });
+
+  it("prevents self-destruction effect on explosive attacks", async() => {
+    const moveToUse = Moves.EXPLOSION;
+    const enemyAbility = Abilities.DAMP;
+
+    vi.spyOn(overrides, "ABILITY_OVERRIDE", "get").mockReturnValue(Abilities.NONE);
+    vi.spyOn(overrides, "MOVESET_OVERRIDE", "get").mockReturnValue([moveToUse]);
+    vi.spyOn(overrides, "OPP_MOVESET_OVERRIDE", "get").mockReturnValue([Moves.SPLASH, Moves.NONE, Moves.NONE, Moves.NONE]);
+    vi.spyOn(overrides, "OPP_SPECIES_OVERRIDE", "get").mockReturnValue(Species.BIDOOF);
+    vi.spyOn(overrides, "OPP_ABILITY_OVERRIDE", "get").mockReturnValue(enemyAbility);
+
+    await game.startBattle();
+
+    game.doAttack(getMovePosition(game.scene, 0, moveToUse));
 
     await game.phaseInterceptor.to(TurnEndPhase);
 
     expect(game.phaseInterceptor.log).toContain("ShowAbilityPhase");
+    expect(game.phaseInterceptor.log).not.toContain("FaintPhase");
+  });
+
+  it("prevents effect of Aftermath", async() => {
+    const moveToUse = Moves.TACKLE;
+    const playerAbility = Abilities.DAMP;
+    const enemyAbility = Abilities.AFTERMATH;
+
+    vi.spyOn(overrides, "ABILITY_OVERRIDE", "get").mockReturnValue(playerAbility);
+    vi.spyOn(overrides, "MOVESET_OVERRIDE", "get").mockReturnValue([moveToUse]);
+    vi.spyOn(overrides, "OPP_MOVESET_OVERRIDE", "get").mockReturnValue([Moves.SPLASH, Moves.NONE, Moves.NONE, Moves.NONE]);
+    vi.spyOn(overrides, "OPP_SPECIES_OVERRIDE", "get").mockReturnValue(Species.BIDOOF);
+    vi.spyOn(overrides, "OPP_ABILITY_OVERRIDE", "get").mockReturnValue(enemyAbility);
+
+    await game.startBattle();
+
+    game.scene.getEnemyParty()[0].hp = 1;
+
+    game.doAttack(getMovePosition(game.scene, 0, moveToUse));
+
+    await game.phaseInterceptor.to(TurnEndPhase);
+
+    expect(game.phaseInterceptor.log).not.toContain("ShowAbilityPhase");
   });
 
   // TODO Test some of the other AbAttrs that use `args`
